747be2d298ce73d7fdd4c9c9f5e65c5cd08281aa
Vitaliy Havrona <vitaliyhavrona@gmail.com>
2023-11-02 19:25:40 +0200

    first commit

---
+++ src/Leadboard.tsx
@@ -0,0 +1,13 @@
+export default function Leadboard() {
+    return (
+        <div className="leadboard">
+            <h2 style={{color: "goldenrod"}}>Leadboard</h2>
+            <div className="playerList">
+                <div className="player">
+                    <p>Vitaliy</p>
+                    <p className="score">18p</p>
+                </div>
+            </div>
+        </div>
+    )
+}
